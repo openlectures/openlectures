1b5843b55d63979ca20ed72a0c91f26f01788ced
Jethro Kuan <jethrokuan95@gmail.com>
2013-06-07 16:12:14 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Openlectures::Application.config.secret_key_base = '823b801a7e59044858d385c5a22a872ab86fff9cfe2d13f0cbae2575ccf43f14c06892194935363284bd780373f02985c56ee8cafea7449b1137929ace2bc2ee'
